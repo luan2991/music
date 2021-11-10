dce68e9fe94904bbe23951a565b911f7f6e3cfae
luan2991 <luan981998@gmail.com>
2021-11-10 16:15:44 +0700

    hello

--- src/components/sidebarright/components/musicstatus/index.jsx
+++ src/components/sidebarright/components/musicstatus/index.jsx
@@ -1,70 +1,96 @@
 import React from 'react';
 // import PropTypes from 'prop-types';
 import { Box } from '@mui/system';
-import { Button, Popover, Slider, Stack } from '@mui/material';
-import { MoreVert, VolumeDownOutlined } from '@mui/icons-material';
+import {
+  Button,
+  IconButton,
+  ListItemIcon,
+  ListItemText,
+  Menu,
+  MenuItem,
+  MenuList,
+  Popover,
+  Slider,
+  Stack,
+} from '@mui/material';
+import {
+  InsertLink,
+  LibraryMusic,
+  MoreVert,
+  MusicNoteRounded,
+  VolumeDownOutlined,
+} from '@mui/icons-material';
 
 // MusicStatus.propTypes = {
 
 // };
 
-function MusicStatus({ open, anchorEl, handlePopoverOpen, handlePopoverClose }) {
+function MusicStatus({
+  open,
+  anchorEl,
+  openMore,
+  anchorElMore,
+  handlePopoverOpen,
+  handlePopoverClose,
+  handlePopoverMoreOpen,
+  handlePopoverMoreClose,
+}) {
   return (
     <Box bgcolor="rgba(0, 0, 0, 0.87)" paddingTop="20px">
       <Box width="272px" margin="auto">
         <Box display="flex" justifyContent="space-between">
-          <Popover
-            sx={{
-              '& .MuiPaper-root': {
-                borderRadius: '25px',
-              },
-            }}
-            open={anchorEl}
-            anchorEl={anchorEl}
-            anchorOrigin={{
-              vertical: 'top',
-              horizontal: 'center',
-            }}
-            transformOrigin={{
-              vertical: 'bottom',
-              horizontal: 'center',
-            }}
-          >
-            <Box
-              onMouseLeave={handlePopoverClose}
-              width={30}
-              bgcolor="rgba(0, 0, 0, 0.87)"
-              display="flex"
-              alignItems="center"
-              justifyContent="center"
-              height="121px"
-            >
-              <Slider
-                sx={{
-                  height: '80px',
-                  '& .MuiSlider-track': {
-                    border: 'none',
-                  },
-                  '& .MuiSlider-thumb': {
-                    backgroundColor: '#fff',
-                  },
-                  '&:hover, &.Mui-focusVisible, &.Mui-active': {
-                    boxShadow: 'none',
-                  },
-                }}
-                size="small"
-                max={100}
-                min={0}
-                aria-label="Small"
-                orientation="vertical"
-                // getAriaValueText={valuetext}
-                defaultValue={30}
-                // onKeyDown={preventHorizontalKeyboardNavigation}
-              />
-            </Box>
-          </Popover>
           <Stack direction="row" sx={{ mb: 1, px: 1 }} alignItems="center" spacing={2}>
             <Box>
+              <Popover
+                sx={{
+                  '& .MuiPopover-paper': {
+                    borderRadius: '25px',
+                    bgcolor: 'rgb(24, 34, 45)',
+                  },
+                }}
+                elevation={3}
+                open={open}
+                anchorEl={anchorEl}
+                anchorOrigin={{
+                  vertical: 'bottom',
+                  horizontal: 'center',
+                }}
+                transformOrigin={{
+                  vertical: 'bottom',
+                  horizontal: 'center',
+                }}
+              >
+                <Box
+                  onMouseLeave={handlePopoverClose}
+                  width={30}
+                  display="flex"
+                  alignItems="flex-end"
+                  justifyContent="center"
+                  height="130px"
+                >
+                  <Stack
+                    direction="column"
+                    justifyContent="flex-end"
+                    alignItems="center"
+                    spacing={1}
+                  >
+                    <Slider
+                      sx={{
+                        height: '80px',
+                      }}
+                      size="small"
+                      max={100}
+                      min={0}
+                      aria-label="Small"
+                      orientation="vertical"
+                      // getAriaValueText={valuetext}
+                      defaultValue={30}
+                      // onKeyDown={preventHorizontalKeyboardNavigation}
+                    />
+                    <VolumeDownOutlined htmlColor="rgba(244,246,248,0.5)" />
+                  </Stack>
+                </Box>
+              </Popover>
               <VolumeDownOutlined
                 onMouseEnter={handlePopoverOpen}
                 htmlColor="rgba(244,246,248,0.5)"
@@ -80,7 +106,55 @@ function MusicStatus({ open, anchorEl, handlePopoverOpen, handlePopoverClose }) 
             >
               Danh sách phát
             </Button>
-            <MoreVert htmlColor="rgba(244,246,248,0.5)" />
+            <Box>
+              <IconButton
+                aria-haspopup="true"
+                aria-expanded={openMore ? 'true' : undefined}
+                color="primary"
+                onClick={handlePopoverMoreOpen}
+              >
+                <MoreVert htmlColor="rgba(244,246,248,0.5)" />
+              </IconButton>
+              <Menu
+                sx={{
+                  '& .MuiMenu-paper': {
+                    bgcolor: 'rgb(24, 34, 45)',
+                  },
+                }}
+                onClose={handlePopoverMoreClose}
+                open={openMore}
+                anchorEl={anchorElMore}
+                anchorOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                transformOrigin={{
+                  vertical: 'bottom',
+                  horizontal: 'right',
+                }}
+              >
+                <MenuList sx={{ color: 'rgba(244,246,248,0.5)' }}>
+                  <MenuItem>
+                    <ListItemText sx={{ paddingRight: '5px' }}>Thêm vào chờ phát</ListItemText>
+                    <ListItemIcon>
+                      <LibraryMusic htmlColor="rgba(244,246,248,0.5)" fontSize="small" />
+                    </ListItemIcon>
+                  </MenuItem>
+                  <MenuItem>
+                    <ListItemText sx={{ paddingRight: '5px' }}>Sao chép Link</ListItemText>
+                    <ListItemIcon>
+                      <InsertLink htmlColor="rgba(244,246,248,0.5)" fontSize="small" />
+                    </ListItemIcon>
+                  </MenuItem>
+                  <MenuItem>
+                    <ListItemText sx={{ paddingRight: '5px' }}>Đi đến bài hát</ListItemText>
+                    <ListItemIcon>
+                      <MusicNoteRounded htmlColor="rgba(244,246,248,0.5)" fontSize="small" />
+                    </ListItemIcon>
+                  </MenuItem>
+                </MenuList>
+              </Menu>
+            </Box>
           </Stack>
         </Box>
       </Box>
